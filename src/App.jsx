5a3ec22c609d6952a975989162907cf8417b2c26
Naik Mubashir <124430100+naikmubashir@users.noreply.github.com>
2023-09-08 12:23:57 +0000

    Personal Details added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Naik Mubashir",
+  title: "Computer Science Undergrad",
+  email: "",
+  gitHub: "naikmubashir",
+  instagram: "",
+  linkedIn: "naikmubashir",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "naik_mubashir",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
