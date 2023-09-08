37b690dbb84bb82da4b36690157b8c0e512dc22a
Naik Mubashir <124430100+naikmubashir@users.noreply.github.com>
2023-09-08 12:25:54 +0000

    Changes made to About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/picture2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "white background with a laptop";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a College Sophomore majoring in CS. I love learning new technologies and I'm a curious about knowing how things work underneath the hood.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Open Source Enthusiast",
+  "Web Development",
+  "Java",
+  "C++",
+  "Data Structure and Algorithm",
+  "Problem Solving",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Exploring different technologies and learning something about everything and everthing about something. I'm open to learning. I love getting involved in global communities and expanding my network. ";
 
 const About = () => {
   return (
